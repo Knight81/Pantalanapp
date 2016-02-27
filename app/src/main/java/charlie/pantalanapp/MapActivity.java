ae2819239a5c3435cadc3373e8bfb94192d03450
Ismael Barros² <razielmine@gmail.com>
2016-02-27 21:02:51 +0100

    la retrofitada toda

--- app/src/main/java/charlie/pantalanapp/MapActivity.java
+++ app/src/main/java/charlie/pantalanapp/MapActivity.java
@@ -6,6 +6,7 @@ import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,6 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
     private static final String TAG = MapActivity.class.getName();
@@ -24,7 +26,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     private LocationProvider mLocationManager;
     private ArrayList<Pantalan> mAvailablePantalanes;
     private boolean isParking = false;
+    UDOOConnection udooConnection = new UDOOConnection();
 
+    UDOOConnection.Listener udooConnectionListener = new UDOOConnection.Listener() {
+        @Override
+        public void gotPositonUpdate(List<Integer> measures) {
+            Log.i("~~~", "Got measures " + measures.get(0) + " " + measures.get(1) + " " + measures.get(2));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        udooConnection.addListener(udooConnectionListener);
+    }
+
+    @Override
+    protected void onPause() {
+        udooConnection.removeListener(udooConnectionListener);
+        super.onPause();
     }
 
 
